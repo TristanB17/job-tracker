91be8bb9979c41435f2c8f1b5ecc97309582cd40
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-09 23:20:46 -0600

    Successfully added contact to company view page

--- spec/features/contacts/user_sees_existing_contacts_for_a_company_spec.rb
+++ spec/features/contacts/user_sees_existing_contacts_for_a_company_spec.rb
@@ -6,7 +6,7 @@ describe 'user sees company contacts' do
     contact_1 = Contact.create(name: 'Auric Goldfinger', position: 'Gold Magnate', email: 'ilovegold@goldbod.com', company_id: 1)
     contact_2 = Contact.create(name: 'Dr. Julius No', position: 'Mad Scientist', email: 'myhandsaremetal@imelted.com', company_id: 1)
 
-    visit company_path(company)
+    visit company_jobs_path(company)
 
     expect(page).to have_content(contact_1.name)
     expect(page).to have_content(contact_1.position)
