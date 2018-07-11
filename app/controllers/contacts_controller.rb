1e309779d8d53945ed7b05e8397302745ca3bb29
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-11 11:54:12 -0600

    final push

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -15,6 +15,29 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+    @contact = Contact.find(params[:id])
+    @company = Company.find(@contact.company_id)
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @contact.update(contact_params)
+    if @contact.save
+      redirect_to company_jobs_path(@company)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:company_id])
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+
+    redirect_to company_jobs_path(@company)
+  end
+
   private
 
   def contact_params
